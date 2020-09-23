9c26ba248de1ff58ed4123f554f79c0e33e526f3
41094 <71751294+41094@users.noreply.github.com>
2020-09-23 15:24:46 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
+basic.showNumber(0)
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
-    basic.pause(100)
-    basic.clearScreen()
-    basic.pause(100)
-    basic.showString("Hello!")
+	
 })
